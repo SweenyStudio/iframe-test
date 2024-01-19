be58c120d2f0227a81b52aed45ef743535e7b0dc
Mac Sweeny <macsweenydev@gmail.com>
2024-01-19 14:25:28 +1000

    Add sharp package and update Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from "astro/config";
+import icon from "astro-icon";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-
-import icon from "astro-icon";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), icon()],
-  output: "server",
-  adapter: vercel()
+  output: "static",
+  adapter: vercel(),
 });
